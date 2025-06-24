cedee747f4581e859b87bc91311b463d15516083
NguyenManhTuan12 <nguyenmanhtuantuan58@gmail.com>
2025-06-24 10:59:29 +0700

    K23CNT2-NguyenManhTuan

---
+++ NmtLesson05Model/Controllers/NmtMemberController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NmtLesson05Model.Models.DataModels;
+
+namespace NmtLesson05Model.Controllers
+{
+    public class NmtMemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult NmtGetMember()
+        {
+            var nmtMember = new NmtMember();
+            nmtMember.NmtMemberID = Guid.NewGuid().ToString();
+            nmtMember.NmtUserName = "AniKmizu2262";
+            nmtMember.NmtFullName = "Nguyễn Mạnh Tuấn";
+            nmtMember.NmtPassword = "pass123";
+            nmtMember.NmtEmail = "nguyenmanhtuantuan58@gmail.com";
+
+            ViewBag.nmtMember = nmtMember;
+            return View();
+        }
+
+    }
+}
---
+++ NmtLesson05Model/Models/DataModels/NmtMember.cs
@@ -0,0 +1,11 @@
+﻿namespace NmtLesson05Model.Models.DataModels
+{
+    public class NmtMember
+    {
+        public string NmtMemberID { get; set; }
+        public string NmtUserName { get; set; }
+        public string NmtFullName { get; set; }
+        public string NmtPassword { get; set; }
+        public string NmtEmail { get; set; }
+    }
+}
